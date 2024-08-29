87386615d5464d7037444b25481cfbc7577c633a
tarax-desing <histor.iade0a100@gmail.com>
2024-08-29 14:47:35 +0200

    AgenciaDigital

--- javaS.js
+++ javaS.js
@@ -1,15 +1,18 @@
+// Array de frases motivadoras
 const frases = [
   "Optimización de contenidos con inteligencia artificial para mejorar el ranking en motores de búsqueda.",
-  "Detección de palabras clave y oportunidades de mejora en tu estrategia de SEO..",
-  "Experiencia en creación de contenido, gestión de redes sociales y seo.",
-  "Automatización de tareas repetitivas como el seguimiento de rankings y la monitorización de errores.",
+  "Detección de palabras clave y oportunidades de mejora en tu estrategia de SEO.",
   "Análisis de grandes cantidades de datos para identificar patrones y tendencias.",
+  "Mejora de la velocidad y rendimiento de tu sitio web para mejorar la experiencia del usuario.",
+  "Creación de contenido de alta calidad y relevante para atraer tráfico orgánico.",
 ];
+
+// Función para mostrar una frase aleatoria
 function mostrarFrase() {
-  var frase = document.getElementById("frase");
-  if (frase.style.display === "none") {
-    frase.style.display = "block";
-  } else {
-    frase.style.display = "none";
-  }
+  // Genera un índice aleatorio basado en la longitud del array
+  const indiceAleatorio = Math.floor(Math.random() * frases.length);
+  // Actualiza el contenido del párrafo con la frase seleccionada
+  document.getElementById("frase").innerText = frases[indiceAleatorio];
 }
+
+setInterval(mostrarFrase, 2000);
