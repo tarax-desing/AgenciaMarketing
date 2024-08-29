6b664ec50a3e8bcc1086e0355ed745d36c88bd92
tarax-desing <histor.iade0a100@gmail.com>
2024-08-29 12:45:45 +0200

    agenciaMarketing

---
+++ javaS.js
@@ -0,0 +1,15 @@
+const frases = [
+  "Optimización de contenidos con inteligencia artificial para mejorar el ranking en motores de búsqueda.",
+  "Detección de palabras clave y oportunidades de mejora en tu estrategia de SEO..",
+  "Experiencia en creación de contenido, gestión de redes sociales y seo.",
+  "Automatización de tareas repetitivas como el seguimiento de rankings y la monitorización de errores.",
+  "Análisis de grandes cantidades de datos para identificar patrones y tendencias.",
+];
+function mostrarFrase() {
+  var frase = document.getElementById("frase");
+  if (frase.style.display === "none") {
+    frase.style.display = "block";
+  } else {
+    frase.style.display = "none";
+  }
+}
